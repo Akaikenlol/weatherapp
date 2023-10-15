08b4a49431107af50f4126399134d25de27b7cf6
Akaikenlol <keithken972@gmail.com>
2023-10-16 00:04:56 +0630

    all good for now

--- components/card.tsx
+++ components/card.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 
 type WeatherData = {
 	name: string;
+
 	main: {
 		temp: number;
 		humidity: number;
@@ -13,10 +14,16 @@ type WeatherData = {
 	wind: {
 		speed: number;
 	};
+	weather: {
+		main: string;
+		description: string;
+	}[];
+	sys: {
+		country: string;
+	};
 };
 
-const Card = ({ lat, lon }: any) => {
-	// const [weather, setWeather] = useState({});
+const Card = () => {
 	const [weather, setWeather] = useState<WeatherData | null>(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
@@ -28,7 +35,7 @@ const Card = ({ lat, lon }: any) => {
 		setLoading(true);
 		try {
 			const response = await axios.get(
-				`${apiUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+				`${apiUrl}/data/2.5/weather?lat=35.41&lon=139.41&units=imperial&appid=${apiKey}`
 			);
 			setWeather(response.data);
 			setLoading(false);
@@ -41,28 +48,10 @@ const Card = ({ lat, lon }: any) => {
 		fetchWeather();
 	}, []);
 
-	// if (!weather) {
-	// 	return (
-	// 		<div className="flex justify-center items-center mt-12">
-	// 			<div className="flex justify-center items-center bg-gradient-to-t from-teal-200 to-emerald-200 rounded-md w-[350px] h-[100px] shadow-md shadow-teal-100">
-	// 				Loading....
-	// 			</div>
-	// 		</div>
-	// 	);
-	// } else {
-	// 	return (
-	// 		<div className="flex justify-center items-center mt-12">
-	// 			<div className="flex justify-center items-center bg-gradient-to-t from-teal-200 to-emerald-200 rounded-md w-[350px] h-[100px] shadow-md shadow-teal-100">
-	// 				{weather.name}
-	// 			</div>
-	// 		</div>
-	// 	);
-	// }
-
 	return (
 		<div className=" flex justify-center items-center mt-12">
 			<div className="bg-gradient-to-t from-teal-200 to-emerald-200 rounded-md w-[350px] h-[400px] shadow-md shadow-teal-100">
-				<div className="flex justify-center items-center text-center flex-col gap-2 mt-10">
+				<div className="flex justify-center items-center text-center flex-col gap-2 ">
 					<Image
 						src="/assets/cloud.png"
 						alt="cloud"
@@ -70,35 +59,38 @@ const Card = ({ lat, lon }: any) => {
 						width={120}
 						height={120}
 					/>
-					<h2 className="text-2xl">City: {weather?.name}</h2>
+					<div className="flex justify-center items-center gap-2 text-center">
+						<h2 className="text-2xl font-bold">{weather?.name} ,</h2>
+						<p className="text-2xl font-bold">{weather?.sys?.country}</p>
+					</div>
 				</div>
-				<div className="flex justify-center items-center text-center mt-5 text-gray-600">
-					<p className="text-3xl">{weather?.main?.temp}</p>
+				<div className="flex justify-center items-center text-center mt-5 gap-2 ">
+					<p className="text-3xl ">{weather?.main?.temp}</p>
 					<p className="text-lg">&deg;F</p>
 				</div>
 
-				<div className="text-gray-600 mt-4 flex justify-center items-center text-center flex-col gap-2">
-					<p className="text-sm">Partly Cloudy</p>
+				<div className=" mt-4 flex justify-center items-center text-center flex-col gap-4">
+					<p className="">{weather?.weather[0]?.main}</p>
 
-					<div className="flex  gap-2">
+					<div className="flex justify-center items-center text-center gap-2">
 						<Image
 							src={"/assets/drop.png"}
 							alt="cloud"
 							className="object-contain "
-							width={20}
-							height={20}
+							width={35}
+							height={35}
 						/>
-						<p className="text-sm">Humidity: {}%</p>
+						<p className="">{weather?.main?.humidity}%</p>
 					</div>
-					<div className="flex  gap-2">
+					<div className="flex justify-center items-center gap-2">
 						<Image
 							src={"/assets/air-flow.png"}
 							alt="cloud"
 							className="object-contain "
-							width={20}
-							height={20}
+							width={35}
+							height={35}
 						/>
-						<p className="text-sm">Wind: {} mph</p>
+						<p className="">{weather?.wind?.speed} mph</p>
 					</div>
 				</div>
 			</div>
--- components/search.tsx
+++ components/search.tsx
@@ -9,12 +9,9 @@ import Card from "./card";
 const Search = () => {
 	const [city, setCity] = useState("");
 	const [temp, setTemp] = useState("");
-	const [humidity, setHumidity] = useState("");
-	const [wind, setWind] = useState("");
-	const [err, setErr] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-	const url = `https://api.openweathermap.org/data/2.5/weather?lat=40.34&lon=-75.90&units=imperial&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.41&lon=139.41&units=imperial&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`;
 
 	const fetchWeather = (e: any) => {
 		e.preventDefault();
@@ -22,7 +19,7 @@ const Search = () => {
 		axios.get(url).then((res) => {
 			setCity(res.data);
 			console.log("City: ", res.data);
-			return <Card lat={res.data.coord.lat} lon={res.data.coord.lon} />;
+			return <Card />;
 		});
 		setTemp("");
 		setLoading(false);
@@ -52,50 +49,6 @@ const Search = () => {
 					/>
 				</button>
 			</div>
-			{/* 
-			<div className=" flex justify-center items-center mt-12">
-				<div className="bg-gradient-to-t from-teal-200 to-emerald-200 rounded-md w-[350px] h-[400px] shadow-md shadow-teal-100">
-					<div className="flex justify-center items-center text-center flex-col gap-2 mt-10">
-						<Image
-							src="/assets/cloud.png"
-							alt="cloud"
-							className="object-contain"
-							width={120}
-							height={120}
-						/>
-						<h2 className="text-2xl">City:</h2>
-					</div>
-					<div className="flex justify-center items-center text-center mt-5 text-gray-600">
-						<p className="text-3xl">{temp}</p>
-						<p className="text-lg">&deg;F</p>
-					</div>
-
-					<div className="text-gray-600 mt-4 flex justify-center items-center text-center flex-col gap-2">
-						<p className="text-sm">Partly Cloudy</p>
-
-						<div className="flex  gap-2">
-							<Image
-								src={"/assets/drop.png"}
-								alt="cloud"
-								className="object-contain "
-								width={20}
-								height={20}
-							/>
-							<p className="text-sm">Humidity: {humidity}%</p>
-						</div>
-						<div className="flex  gap-2">
-							<Image
-								src={"/assets/air-flow.png"}
-								alt="cloud"
-								className="object-contain "
-								width={20}
-								height={20}
-							/>
-							<p className="text-sm">Wind: {wind} mph</p>
-						</div>
-					</div>
-				</div>
-			</div> */}
 		</div>
 	);
 };
